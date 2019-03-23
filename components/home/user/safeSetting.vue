114f58eb1999d96a64a0a0632dc28370e87f84bd
JiPai <github@jipai.moe>
2019-03-23 23:17:02 +0800

    新增用户安全设置页面组件

---
+++ components/home/user/safeSetting.vue
@@ -0,0 +1,91 @@
+<template lang="pug">
+    el-row(:gutter='20' )
+        el-col(:span='10')
+            .grid-content
+                el-form(:model="ruleForm" :rules="rules" :hide-required-asterisk="true" ref="ruleForm" label-width="70px")
+                    el-form-item(label="邮箱" prop="phone")
+                        el-input(v-model="ruleForm.phone")
+                    el-form-item(label="手机号" prop="classic")
+                        el-input(v-model="ruleForm.phone")
+                    el-form-item(label="密码" prop="phone")
+                        el-input(v-model="ruleForm.phone")
+                    el-form-item
+                        el-button(type="primary" style="width:100%" @click="submitForm('ruleForm')") 修改
+</template>
+<script>
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      ruleForm: {
+        email: '',
+        password: ''
+      },
+      rules: {
+        email: [
+          { required: true, message: '请输入登录邮箱', trigger: 'blur' },
+          {
+            type: 'email',
+            required: true,
+            message: '邮箱格式不正确',
+            trigger: 'change'
+          },
+          { max: 255, message: '长度不得多于255个字符', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    // async function fetchSomething () {
+    //   let ip = await this.$axios.$get('http://icanhazip.com');
+    //   console.log(ip);
+    // };
+    async login() {
+      let username = this.ruleForm.email
+      let password = this.ruleForm.password
+      let { data } = await axios.post(`/api/user/login`, {
+        email: username,
+        password: password
+      })
+      console.log(data.loginStatus === '0')
+      if (data.loginStatus === '0') {
+        console.log('wsl')
+        this.$notify.error({
+          title: '登录失败',
+          message: data.errMsg
+        })
+      }
+      if (data.loginStatus === '1') {
+        this.$notify({
+          title: '登录成功~',
+          message:
+            '身份验证通过，用户ID为' + data.userId + '，现在启动页面转向。',
+          type: 'success',
+          onClose: this.redirectpage
+        })
+      }
+      console.log(data)
+    },
+    redirectpage() {
+      window.location.href = '/home/portal'
+    },
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          this.login()
+          this.$notify.info({
+            title: '登录中...',
+            message: '连接服务器中'
+          })
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields()
+    }
+  }
+}
+</script>
