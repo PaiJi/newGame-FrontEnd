9516cdf94d1505bdaf1d247a3827aff719a469ed
JiPai <github@jipai.moe>
2019-03-23 23:17:40 +0800

    新增管理员级别控制组件

---
+++ components/home/setting.vue
@@ -0,0 +1,46 @@
+<template lang="pug">
+    .container
+        el-row
+            el-col
+                h2 系统设置
+                hr
+        el-collapse.collapse-padding
+            el-collapse-item(title="系统控制")
+                el-row
+                    el-col
+                        el-switch(v-model="systemPowerSwitch" active-text="系统开放" inactive-text="系统关闭")
+            el-collapse-item(title="超级管理员控制" )
+                el-row(:gutter='20')
+                    el-col(:span='4')
+                        el-input(v-model="userid" placeholder="输入用户ID")
+                    el-col(:span='4')
+                        el-button(type="danger") 添加超级管理员
+</template>
+<style lang="scss">
+.collapse-padding {
+  .el-collapse-item__header {
+    padding: 0px 10px;
+  }
+  .el-row {
+    padding: 0px 10px;
+  }
+}
+hr {
+  border-color: rgb(244, 244, 244);
+  border-width: 1px;
+  margin-top: 10px;
+  margin-bottom: 20px;
+}
+</style>
+
+
+<script>
+export default {
+  data() {
+    return {
+      systemPowerSwitch: false,
+      userid: ''
+    }
+  }
+}
+</script>
