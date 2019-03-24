1d518ef0777e0735ec8bf22f55e6399df50fe9d5
JiPai <github@jipai.moe>
2019-03-24 12:25:27 +0800

    clubList组件初始化

---
+++ components/home/clubList.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+    el-row
+        el-col
+            el-table(:data="clubList")
+                el-table-column(label="社团名称" prop="name" width="")
+                el-table-column(label="社团简介" prop="intro" width="")
+                el-table-column(label="社团人数" prop="members" width="")
+                el-table-column(label="注册时间" prop="create_time" width="")
+</template>
+<script>
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      clubList: []
+    }
+  },
+  mounted() {
+    this.fillData()
+  },
+  methods: {
+    async fillData() {
+      console.log('Start GET Data')
+      let { data } = await axios.get('/api/club/clublist')
+      console.log(data)
+      this.clubList = data
+    }
+  }
+}
+</script>
