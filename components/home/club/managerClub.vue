fd3f38cee8d88ffdea923ad0d63d65d0ca51d9d3
JiPai <github@jipai.moe>
2019-05-16 21:57:43 +0800

    用4空格缩进重新格式化两个错误以2空格缩进的文件。

--- components/home/club/managerClub.vue
+++ components/home/club/managerClub.vue
@@ -1,65 +1,90 @@
 <template lang="pug">
     .container
-      .clubList(v-if="whichIsShow=='list'")
-        el-row
-            el-col
-                el-table(:data="clubList" @current-change="showClubDetailPanel")
-                    el-table-column(label="社团名称" prop="name" width="")
-                    el-table-column(label="社团简介" prop="intro" width="")
-                    el-table-column(label="社团人数" prop="members" width="")
-                    el-table-column(label="注册时间" prop="create_time" width="")
-      .clubPanel(v-if="whichIsShow=='clubPanel'")
-        el-tabs(type="border-card")
-          el-tab-pane(label="修改社团信息").clubDetail
+        .clubList(v-if="whichIsShow=='list'")
             el-row
-              el-col(:md="4")
-                p 社团名称
-              el-col(:md="20")
-                p {{selectClub.name}}
-            el-row
-              el-col(:md="4")
-                p 社团简介
-              el-col(:md="20")
-                p {{selectClub.intro}}
-            el-row
-              el-col(:md="4")
-                p 社团状态
-              el-col(:md="20")
-                p {{selectClub.status}}
-            el-row
-              el-col(:md="4")
-                p 社团成员数
-              el-col(:md="20")
-                p {{selectClub.members}}
-            el-row
-              el-col(:md="4")
-                p 社团类别
-              el-col(:md="20")
-                p {{selectClub.sort}}
-            el-row
-              el-col(:md="4")
-                p 社团所属部门
-              el-col(:md="20")
-                p {{selectClub.belong}}
-            el-row
-              el-col(:md="4")
-                p 社团创建时间
-              el-col(:md="20")
-                p {{selectClub.create_time}}
-            el-row
-              el-col(:md="4")
-                p 社团加入模式
-              el-col(:md="20")
-                p {{selectClub.join_mode}}
-            el-row
-              el-col(:md="4")
-                p 社团ID
-              el-col(:md="20")
-                p {{selectClub.id}}
-          el-tab-pane(label="修改纳新问卷")
-            editQuestion(v-bind:selectClub="selectClub.id")
-          el-tab-pane(label="审批管理") 审批管理
-
+                el-col
+                    el-table(:data="clubList" @current-change="showClubDetailPanel")
+                        el-table-column(label="社团名称" prop="name" width="")
+                        el-table-column(label="社团简介" prop="intro" width="")
+                        el-table-column(label="社团人数" prop="members" width="")
+                        el-table-column(label="注册时间" prop="create_time" width="")
+        .clubDetailContainer(v-if="whichIsShow=='clubPanel'")
+            el-row.clubStatusBar
+                el-col(:span='4')
+                    el-button(@click="whichIsShow='list'") 返回列表
+                el-col(:span='20')
+                    h4 {{selectClub.name}}
+                    span 详细信息
+            .clubPanel
+                el-tabs(type="border-card")
+                    el-tab-pane(label="修改社团信息").clubDetail
+                        el-form(:model="updateClubForm" label-width="100px")
+                            el-row
+                                el-col(:span="12")
+                                    el-form-item(label="社团名称" prop="name")
+                                        el-input(v-model="updateClubForm.clubName")
+                            el-row
+                                el-col(:span="12")
+                                    el-form-item(label="LOGO地址" prop="imgUrl")
+                                        el-input(v-model="updateClubForm.imgUrl")
+                            el-row
+                                el-col(:span="12")
+                                    el-form-item(label="社团简介" prop="intro")
+                                        el-input(type="textarea" :row=8 placeholder="随便写点什么，让大家了解你的社团" v-model="updateClubForm.intro")
+                            el-row
+                                el-col(:span="12")
+                                    el-form-item(label="社团归属" prop="clubBelong")
+                                        el-select(placeholder="请选择社团归属" v-model="updateClubForm.clubBelong")
+                                            el-option(label="电子信息工程" value="2")
+                                            el-option(label="团委" value="1")
+                            el-row
+                                el-col(:span="12")
+                                    el-form-item(label="社团分类" prop="clubSort")
+                                        el-select(placeholder="请选择社团类别" v-model="updateClubForm.clubSort")
+                                            el-option(label="技术类" value="1")
+                                            el-option(label="娱乐类" value="2")
+                            el-row
+                                el-col(:span="12")
+                                    el-form-item(label="社团负责人" prop="clubadmin")
+                                        el-input(v-model="updateClubForm.clubAdmin" placeholder="输入该用户的UID")
+                            el-row
+                                el-col
+                                    el-form-item(label="加入方式" prop="joinmode")
+                                        el-radio-group(v-model="updateClubForm.joinMode")
+                                            el-radio(label="1") 人工审核
+                                            el-radio(label="2" disabled) 仅邀请
+                                            el-radio(label="3") 无限制
+                                            el-radio(label="0") 不允许新成员
+                            el-row
+                                el-col
+                                    el-form-item(label="社团状态" prop="status")
+                                        el-radio-group(v-model="updateClubForm.status")
+                                            el-radio(label="2") 筹备中
+                                            el-radio(label="1" disabled) 正常运营
+                                            el-radio(label="3" disabled) 运营异常
+                                            el-radio(label="0" disabled) 已注销
+                            el-row
+                                el-col
+                                    el-form-item
+                                        el-button(type="primary" @click="updateClubInfo") 更新信息
+                    el-tab-pane(label="修改纳新问卷")
+                        editQuestion(v-bind:selectClub="selectClub.id")
+                    el-tab-pane(label="审批管理")
+                        el-table(:data="applyList" @current-change="showApplyModal")
+                            el-table-column(label="姓名" prop="userInfo.username")
+                            el-table-column(label="性别" prop="userInfo.gender")
+                            el-table-column(label="电话" prop="userInfo.phone")
+                            el-table-column(label="QQ" prop="userInfo.qq")
+                            el-table-column(label="WECHAT" prop="userInfo.wechat")
+                            el-table-column(label="学院" prop="userInfo.department")
+                            el-table-column(label="专业" prop="userInfo.major")
+                        el-dialog(title="表单详情" :visible.sync="applyContentDialogVisible" width="80%")
+                            el-form(label-position="top")
+                                el-form-item(v-for="(item,index) in applyContent" :label="index+'.'+item.question")
+                                    p 答案：{{item.answer}}
+                            span(slot="footer")
+                                el-button(type="success" @click="handleApply(true)" :disabled="selectApply.status!=`待审核`") 通过
+                                el-button(type="danger" @click="handleApply(false)" :disabled="selectApply.status!=`待审核`") 拒绝
 
 </template>
 <script>
--- components/home/club/myClubList.vue
+++ components/home/club/myClubList.vue
@@ -1,69 +1,93 @@
 <template lang="pug">
     .container
-      .clubList(v-if="whichIsShow=='list'")
-        el-row
-            el-col
-                el-table(:data="clubList" @current-change="showClubDetailPanel")
-                    el-table-column(label="社团名称" prop="name" width="")
-                    el-table-column(label="社团简介" prop="intro" width="")
-                    el-table-column(label="社团人数" prop="members" width="")
-                    el-table-column(label="注册时间" prop="create_time" width="")
-      .clubPanel(v-if="whichIsShow=='clubPanel'")
-        el-tabs(type="border-card")
-          el-tab-pane(label="社团详情").clubDetail
+        .clubList(v-if="whichIsShow=='list'")
             el-row
-              el-col(:md="4")
-                p 社团名称
-              el-col(:md="20")
-                p {{selectClub.name}}
-            el-row
-              el-col(:md="4")
-                p 社团简介
-              el-col(:md="20")
-                p {{selectClub.intro}}
-            el-row
-              el-col(:md="4")
-                p 社团状态
-              el-col(:md="20")
-                p {{selectClub.status}}
-            el-row
-              el-col(:md="4")
-                p 社团成员数
-              el-col(:md="20")
-                p {{selectClub.members}}
-            el-row
-              el-col(:md="4")
-                p 社团类别
-              el-col(:md="20")
-                p {{selectClub.sort}}
-            el-row
-              el-col(:md="4")
-                p 社团所属部门
-              el-col(:md="20")
-                p {{selectClub.belong}}
-            el-row
-              el-col(:md="4")
-                p 社团创建时间
-              el-col(:md="20")
-                p {{selectClub.create_time}}
-            el-row
-              el-col(:md="4")
-                p 社团加入模式
-              el-col(:md="20")
-                p {{selectClub.join_mode}}
-            el-row
-              el-col(:md="4")
-                p 社团ID
-              el-col(:md="20")
-                p {{selectClub.id}}
-          el-tab-pane(label="社团活动") 社团详情
-          el-tab-pane(label="社团考勤") 社团详情
-          el-tab-pane(label="社团通讯录")
-            el-table(:data="clubContact")
-              el-table-column(label="姓名" prop="username" width="")
-              el-table-column(label="昵称" prop="nickname" width="")
-              el-table-column(label="手机号码" prop="phone" width="")
-              el-table-column(label="角色" prop="clubRole" width="")
+                el-col
+                    el-table(:data="clubList" @current-change="showClubDetailPanel")
+                        el-table-column(label="社团名称" prop="clubInfo.name" width="")
+                        el-table-column(label="社团简介" prop="clubInfo.intro" width="")
+                        el-table-column(label="社团人数" prop="clubInfo.members" width="")
+                        el-table-column(label="注册时间" prop="clubInfo.create_time" width="")
+        .clubPanel(v-if="whichIsShow=='clubPanel'")
+            el-tabs(type="border-card")
+                el-tab-pane(label="社团详情").clubDetail
+                    el-row
+                        el-col(:md="4")
+                            p 社团名称
+                        el-col(:md="20")
+                           p {{selectClub.name}}
+                    el-row
+                        el-col(:md="4")
+                            p 社团简介
+                        el-col(:md="20")
+                            p {{selectClub.intro}}
+                    el-row
+                        el-col(:md="4")
+                            p 社团状态
+                        el-col(:md="20")
+                            p {{selectClub.status}}
+                    el-row
+                        el-col(:md="4")
+                            p 社团成员数
+                        el-col(:md="20")
+                            p {{selectClub.members}}
+                    el-row
+                        el-col(:md="4")
+                            p 社团类别
+                        el-col(:md="20")
+                            p {{selectClub.sort}}
+                    el-row
+                        el-col(:md="4")
+                            p 社团所属部门
+                        el-col(:md="20")
+                            p {{selectClub.belong}}
+                    el-row
+                        el-col(:md="4")
+                            p 社团创建时间
+                        el-col(:md="20")
+                            p {{selectClub.create_time}}
+                    el-row
+                        el-col(:md="4")
+                            p 社团加入模式
+                        el-col(:md="20")
+                            p {{selectClub.join_mode}}
+                    el-row
+                        el-col(:md="4")
+                            p 社团ID
+                        el-col(:md="20")
+                            p {{selectClub.id}}
+                el-tab-pane(label="社团活动")
+                        el-table(:data='clubActivityList', style='width: 100%')
+                            el-table-column(label='活动名称', width='180')
+                                template(slot-scope='scope')
+                                    i.el-icon-time
+                                    span(style='margin-left: 10px') {{ scope.row.name }}
+                            //- el-table-column(label='活动简介', width='180')
+                            //-     template(slot-scope='scope')
+                            //-         span {{ scope.row.intro }}
+                            el-table-column(label='活动类型' width='180')
+                                template(slot-scope='scope')
+                                    .name-wrapper(slot='reference')
+                                        el-tag(size='medium') {{ scope.row.type }}
+                            el-table-column(label='活动状态' width='180')
+                                template(slot-scope='scope')
+                                    .name-wrapper(slot='reference')
+                                        el-tag(size='medium') {{ scope.row.status }}
+                            el-table-column(label='开始时间' width='180')
+                                template(slot-scope='scope')
+                                    span(style='margin-left: 10px') {{ scope.row.start_time }}
+                            el-table-column(label='结束时间' width='180')
+                                template(slot-scope='scope')
+                                    span(style='margin-left: 10px') {{ scope.row.end_time }}
+                            el-table-column(label='操作')
+                                template(slot-scope='scope')
+                                    el-button(size='mini', type='success', @click='handleDelete(scope.row.activity_id)') 加入
+                el-tab-pane(label="社团通讯录")
+                    el-table(:data="clubContact")
+                        el-table-column(label="姓名" prop="username" width="")
+                        el-table-column(label="昵称" prop="nickname" width="")
+                        el-table-column(label="手机号码" prop="phone" width="")
+                        el-table-column(label="角色" prop="clubRole" width="")
 
 
 </template>
